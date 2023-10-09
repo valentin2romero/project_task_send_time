4dd61f7f5c7b98072e931d590d47634a9d3e92b0
Valentin <valentin2romero@gmail.com>
2023-10-09 21:48:11 +0000

    initial module

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Project Task Sent Time",
+    "summary": """Project Task Sent Time""",
+    "version": "16.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Valentin Romero",
+    "website": "",
+    "depends": [
+        "base", "project"
+    ],
+    "data": [
+        'views/project_task.xml',
+    ],
+}
